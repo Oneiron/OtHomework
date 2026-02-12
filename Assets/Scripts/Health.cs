ab038d96912a70abc9d26153e3b265769002a5e9
Oneiron <82400714+Oneiron@users.noreply.github.com>
2026-02-13 01:37:03 +0300

    Initial commit

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace MyGame
+{
+    public class Health : MonoBehaviour
+    {
+        public int health = 100;
+        public bool isDeath = false;
+
+        public void setDamage(int damage)
+        {
+            health -= damage;
+            if(health<0)
+                health = 0;
+            if(health==0)
+                isDeath = true;
+            else isDeath = false;
+        
+        }
+
+    }
+}
